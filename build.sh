797c6bc788ecb10fbad76c7aed35b6b1fe04bb70
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-01-19 20:52:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+#sync
 rm -rf .repo
 rm -rf *
 repo init -u repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
 git clone https://github.com/sundrams-playground/local_manifests.git -b tiramisu .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
+# fetch submodule
+cd kernel/samsung/m30s
+git submodule init && git submodule update
+
+# build
 . build/envsetup.sh
 lunch lineage_m307f-userdebug
 mka bacon
