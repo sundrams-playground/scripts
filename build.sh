1fd243a75c93328e50806055781652de679af36e
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-01-20 19:01:15 +0530

    fix
    :p

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #sync
 rm -rf .repo
 rm -rf *
-repo init -u repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
 git clone https://github.com/sundrams-playground/local_manifests.git -b tiramisu .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
