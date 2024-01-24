1f4fa4a63636dc9f096a39abc9336fabc2cde7d4
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-01-24 11:14:00 +0530

    initial matirxx buildbot script

---
+++ matrix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#sync
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+git clone https://github.com/naz664/treble_manifest.git .repo/local_manifests  -b 14
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+
+#apply patches
+bash patches/apply-patches.sh .
+
+#Generating Rom Makefile
+cd device/phh/treble
+bash generate.sh crDroid
+
+#compile
+. build/envsetup.sh
+lunch treble_arm64_bgN-userdebug 
+make systemimage -j$(nproc --all)
+
+#compress img
+cd out/target/product/tdgsi_arm64_ab
+xz -z -k system.img 
