aaeb4ceab30fc59dc6dbc0a12759ef5fc2e063ef
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-01-24 11:17:14 +0530

    delete everything hehe

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #sync
-rm -rf .repo/local_manifests
+rm -rf *
 repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
 git clone https://github.com/sundrams-playground/local_manifests.git -b tiramisu .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
