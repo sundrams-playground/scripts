747bf2856d19c939265755edc7f3861d1d8c92cb
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-03-02 13:07:13 +0530

    rm -rf local manifest

--- rising.sh
+++ rising.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # init and sync
+rm -rf .repo/local_manifests
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 git clone https://github.com/xyz-sundram/local_manifest.git -b rising .repo/local_manifests
 repo sync --current-branch --no-tags -j$(nproc --all) --force-sync
@@ -9,3 +10,6 @@ repo sync --current-branch --no-tags -j$(nproc --all) --force-sync
 source build/envsetup.sh
 lunch rising_tulip-userdebug
 m bacon
+
+# crave build command
+# crave run --no-patch "rm -rf rising.sh && wget https://raw.githubusercontent.com/sundrams-playground/scripts/main/rising.sh && chmod +x rising.sh && bash rising.sh"
