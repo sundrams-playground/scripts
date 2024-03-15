3bd9d7dcaf1da30ba8483d64cbcdb9f4050eca49
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-03-15 10:23:59 +0530

    get the fk build this time

--- rising.sh
+++ rising.sh
@@ -6,6 +6,11 @@ repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 git clone https://github.com/xyz-sundram/local_manifest.git -b rising .repo/local_manifests
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 
+#apply patch for custom hal
+cd vendor/lineage
+curl https://github.com/xyz-sundram/android_vendor_lineage/commit/b6971d2c1293ad8c8eb3eba74d40eec8a6b084b2.patch | git am
+cd ../..
+
 # build rom
 source build/envsetup.sh
 lunch rising_tulip-userdebug
