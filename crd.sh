9033694c5deba54a0215beaf0123d1152d0b9d12
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-04-15 09:28:59 +0530

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -1,5 +1,6 @@
 ## sync
 repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
+rm -rf .repo/local_manifests
 git clone https://github.com/sundrams-playground/local_manifests.git -b tiramisu .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)  
 
