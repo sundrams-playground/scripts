e0c772f39c1cb9826a5d074fdfc3376edc3790cd
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-04-28 19:13:49 +0530

    one ends and another begins

--- build.sh
+++ build.sh
@@ -9,3 +9,6 @@ repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune 
 . build/envsetup.sh
 lunch lineage_m307f-userdebug
 m evolution
+
+lunch lineage_m307fn-userdebug
+m evolution
