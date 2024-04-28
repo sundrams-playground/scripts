eb2f18b09ab5785de2cc75b6bdb9ec1938614ac2
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-04-28 12:44:46 +0530

    build evo

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #sync
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs
 git clone https://github.com/sundrams-playground/local_manifests.git -b udc .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # build
 . build/envsetup.sh
-lunch lineage_m307f-ap1a-eng
+lunch lineage_m307f-userdebug
 mka bacon
